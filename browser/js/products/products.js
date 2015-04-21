5ba2a4abc62cfe2cb0b39b8ac2dfe2708e4f0a7b
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-04-21 17:37:38 -0400

    added routes for products page

---
+++ browser/js/products/products.js
@@ -0,0 +1,33 @@
+'use strict';
+app.config(function ($stateProvider) {
+
+    // Register our *about* state.
+    $stateProvider.state('products', {
+        url: '/products',
+        controller: 'ProductsController',
+        templateUrl: 'js/products/products.html'
+    });
+
+});
+
+app.controller('ProductsController', function ($scope, ProductFactory) {
+
+	ProductFactory.getShoes().then(function (shoes) {
+		$scope.shoes = shoes.data;
+	});
+
+});
+
+app.factory('ProductFactory', function ($http) {
+
+    var getShoes = function () {
+        return $http.get('/api/products/shoes').then(function (response) {
+            return response.data;
+        });
+    };
+
+    return {
+        getShoes: getShoes
+    };
+
+});
