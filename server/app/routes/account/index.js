cebf3d76a39baac5d28bfeccffeb3e9dde196970
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-04-23 18:09:58 -0400

    account page changes - minor

--- server/app/routes/account/index.js
+++ server/app/routes/account/index.js
@@ -5,12 +5,11 @@ var mongoose = require('mongoose');
 var UserModel = mongoose.model('User');
 module.exports = router;
 
-router.post('/userinfo', function (req, res) {
+router.get('/userinfo', function (req, res) {
 
-  var findUser = UserModel.findOne({ _id: req.body.data._id }).exec();
-
+  var findUser = UserModel.findOne({ _id: req.user._id }).exec();
   findUser.then(function(user) {
-		res.status(200).send( _.omit(user.toJSON(),['password','salt']) );
+	res.status(200).send( _.omit(user.toJSON(),[,'salt']) );
   });
 });
 
--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -8,8 +8,8 @@ var Product = mongoose.model("Product");
 router.get('/shoes', function (req, res) {
 	console.log("REQUEST", req.body);
 
- 	Product.find({category: {name: }}, function (err, shoes){
- 		console.log("shoes : ", shoes)
+ 	Product.find({category: {name: 'Sam' }}, function (err, shoes){
+ 		console.log("shoes : ", shoes);
  		res.send(shoes);
  	});
 
