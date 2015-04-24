d03a165925b321b4e2b6748b25c688a571afb3d7
carlos-r-mendoza <clos.mendoza@gmail.com>
2015-04-24 11:22:15 -0400

    removed repetitive js/product, which had same logic found in js/products -Carlos

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -37,3 +37,25 @@ app.factory('ProductFactory', function ($http) {
     };
 
 });
+
+app.controller("PanelController", function ($scope) {
+    $scope.tab = 1;
+
+    $scope.selectedTab = function(setTab) {
+        $scope.tab = setTab;
+    };
+
+    $scope.isSelected = function(checkTab) {
+        return $scope.tab === checkTab;
+    };
+
+});
+
+app.controller("ReviewController", function ($scope) {
+    $scope.review = {};
+
+    $scope.addReview = function(product) {
+        product.reviews.push($scope.review);
+        $scope.review = {};
+    };
+});
--- seed.js
+++ seed.js
@@ -71,20 +71,20 @@ var seedCart = function () {
 };
 
 connectToDb.then(function () {
-  getCurrentUserData().then(function (users) {
-    if (users.length === 0) {
-      return seedUsers();
-    } else {
-      console.log(chalk.magenta('Seems to already be user data, exiting!'));
-      process.kill(0);
-    }
-  }).then(function () {
-    console.log(chalk.green('Seed successful!'));
-    process.kill(0);
-  }).catch(function (err) {
-    console.error(err);
-    process.kill(1);
-  });
+  // getCurrentUserData().then(function (users) {
+  //   if (users.length === 0) {
+  //     return seedUsers();
+  //   } else {
+  //     console.log(chalk.magenta('Seems to already be user data, exiting!'));
+  //     process.kill(0);
+  //   }
+  // }).then(function () {
+  //   console.log(chalk.green('Seed successful!'));
+  //   process.kill(0);
+  // }).catch(function (err) {
+  //   console.error(err);
+  //   process.kill(1);
+  // });
 
   getCurrentProduct().then(function (products) {
     return seedProducts();
