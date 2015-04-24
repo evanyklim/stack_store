80c1f2b79a4857e3fe440c506787cf01f8e0e2f2
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-04-24 12:44:46 -0400

    pre merge commit

---
+++ browser/js/admin/admin.js
@@ -0,0 +1,15 @@
+'use strict';
+app.config(function ($stateProvider) {
+
+	$stateProvider.state('admin', {
+		url: '/admin',
+		controller: 'AdminController',
+		templateUrl: 'js/admin/admin.html'
+	});
+});
+
+app.controller('AdminController', function ($scope) {
+
+	$scope.items = { data: ['item 1', 'item 2', 'item 3'] }
+
+});
