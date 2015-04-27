72b2dce31143057e77dab35477106df867a48ed7
carlos-r-mendoza <clos.mendoza@gmail.com>
2015-04-27 16:37:38 -0400

    Facebook login now works! -Carlos

--- server/env/development.js
+++ server/env/development.js
@@ -7,9 +7,9 @@ module.exports = {
     "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
   },
   "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+    "clientID": "1383702118625353",
+    "clientSecret": "44c52eb549f73836d59ce5345df86cf6",
+    "callbackURL": "http://localhost:1337/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
